f994c3a3d3dd7f7dc66d95cb0c1a055607dddd11
aaron-613 <github@aaronlee.ca>
2024-08-06 10:01:50 -0700

    removed JUnit references, fixed var names

--- src/main/java/com/solace/samples/javarto/howtos/HowToUseQueuePartitionKey.java
+++ src/main/java/com/solace/samples/javarto/howtos/HowToUseQueuePartitionKey.java
@@ -2,22 +2,10 @@
  * Copyright 2004-2021 Solace Corporation. All rights reserved.
  *
  */
-package com.solace.samples.javarto.snippets;
+package com.solace.samples.javarto.howtos;
 
-import java.nio.ByteBuffer;
-import java.util.logging.Level;
-
-import com.solacesystems.solclientj.core.SolEnum;
-import com.solacesystems.solclientj.core.Solclient;
 import com.solacesystems.solclientj.core.SolclientException;
-import com.solacesystems.solclientj.core.event.FlowEventCallback;
-import com.solacesystems.solclientj.core.event.SessionEventCallback;
-import com.solacesystems.solclientj.core.handle.ContextHandle;
-import com.solacesystems.solclientj.core.handle.FlowHandle;
 import com.solacesystems.solclientj.core.handle.MessageHandle;
-import com.solacesystems.solclientj.core.handle.SessionHandle;
-import com.solacesystems.solclientj.core.resource.Destination;
-import com.solacesystems.solclientj.core.resource.Queue;
 
 public class HowToUseQueuePartitionKey {
 
@@ -25,29 +13,33 @@ public class HowToUseQueuePartitionKey {
      * that does not already have a queue partition key set.
      */
     public void howToApplyQueuePartitionKey(MessageHandle messageHandle, String queuePartitionKey) throws SolclientException {
-        assertNull(String.format("Expected to be passed a MessageHandle without a queue partition key, but instead "
-                + "the queue partition key was set to `%s`",
-                messageHandle.getQueuePartitionKey()),
-            messageHandle.getQueuePartitionKey());
-        messageHandle.setQueuePartitionKey(queueParitionKey);
-        assertEquals(String.format("The configured queue partition key should have matched the specified queue partition key, "
-            + "but instead the configured key was `%s`, and the specified key was `%s`.",
-            messageHandle.getQueuePartitionKey(), queuePartitionKey),
-        messageHandle.getQueuePartitionKey(), queuePartitionKey);
+    	if (messageHandle.getQueuePartitionKey() != null) {
+    		throw new AssertionError(String.format("Expected to be passed a MessageHandle without a queue partition key, "
+    				+ "but instead the queue partition key was set to `%s`",
+                    messageHandle.getQueuePartitionKey()));
+    	}
+        messageHandle.setQueuePartitionKey(queuePartitionKey);
+        if (!messageHandle.getQueuePartitionKey().equals(queuePartitionKey)) {
+    		throw new AssertionError(String.format("The configured queue partition key should have matched the specified queue partition key, "
+    	            + "but instead the configured key was `%s`, and the specified key was `%s`.",
+    	            messageHandle.getQueuePartitionKey(), queuePartitionKey));
+        }
     }
 
     /* This method demonstrates how to set/overwrite the queue partition key on a message
      * that already has a queue partition key set.
      */
     public void howToOverwriteQueuePartitionKey(MessageHandle messageHandle, String queuePartitionKey) throws SolclientException {
-        assertNotNull(String.format("Expected to be passed a MessageHandle with a queue partition key, but instead "
-                + "the queue partition key was null"),
-            messageHandle.getQueuePartitionKey());
-        messageHandle.setQueuePartitionKey(queueParitionKey);
-        assertEquals(String.format("The configured queue partition key should have matched the specified queue partition key, "
-            + "but instead the configured key was `%s`, and the specified key was `%s`.",
-            messageHandle.getQueuePartitionKey(), queuePartitionKey),
-        messageHandle.getQueuePartitionKey(), queuePartitionKey);
+    	if (messageHandle.getQueuePartitionKey() == null) {
+    		throw new AssertionError("Expected to be passed a MessageHandle with a queue partition key, but instead "
+                    + "the queue partition key was null");
+    	}
+        messageHandle.setQueuePartitionKey(queuePartitionKey);
+        if (!messageHandle.getQueuePartitionKey().equals(queuePartitionKey)) {
+        	throw new AssertionError(String.format("The configured queue partition key should have matched the specified queue partition key, "
+                    + "but instead the configured key was `%s`, and the specified key was `%s`.",
+                    messageHandle.getQueuePartitionKey(), queuePartitionKey));
+        }
     }
 
     /* This method demonstrates how to retrieve the queue partition key from a message
@@ -55,8 +47,9 @@ public class HowToUseQueuePartitionKey {
      */
     public String howToGetPresentQueuePartitionKey(MessageHandle messageHandle) throws SolclientException {
         String queuePartitionKey = messageHandle.getQueuePartitionKey();
-        assertNotNull(String.format("The queue partition key should not have been null."),
-            queuePartitionKey);
+        if (queuePartitionKey == null) {
+        	throw new AssertionError("The queue partition key should not have been null.");
+        }
         return queuePartitionKey;
     }
 
@@ -64,33 +57,37 @@ public class HowToUseQueuePartitionKey {
      * that does not have the queue partition key set.
      */
     public void howToGetAbsentQueuePartitionKey(MessageHandle messageHandle) throws SolclientException {
-        assertNull(String.format("The queue partition key should have been null, but instead was %s.",
-                messageHandle.getQueuePartitionKey()),
-            messageHandle.getQueuePartitionKey());
+    	if (messageHandle != null) {
+    		throw new AssertionError(String.format("The queue partition key should have been null, but instead was %s.",
+                    messageHandle.getQueuePartitionKey()));
+    	}
     }
 
     /* This method demonstrates how to delete the queue partition key from a message
      * that already has the queue partition key set.
      */
     public void howToDeletePresentQueuePartitionKey(MessageHandle messageHandle) throws SolclientException {
-        assertNotNull(String.format("Expected to be passed a MessageHandle with a present queue partition key, but "
-                + "instead, the queue partition key was absent."),
-            messageHandle.getQueuePartitionKey());
+    	if (messageHandle.getQueuePartitionKey() == null) {
+    		throw new AssertionError("Expected to be passed a MessageHandle with a present queue partition key, but "
+                    + "instead, the queue partition key was absent.");
+    	}
         messageHandle.deleteQueuePartitionKey();
-        assertNull(String.format("Expected the call to `deleteQueuePartitionKey()` to succeed, causing the queue "
-            + "partition key to be set to null, but instead the queue partition key after deletion is `%s`."),
-            messageHandle.getQueuePartitionKey(),
-        messageHandle.getQueuePartitionKey());
+        if (messageHandle.getQueuePartitionKey() != null) {
+        	throw new AssertionError(String.format("Expected the call to `deleteQueuePartitionKey()` to succeed, causing the queue "
+                    + "partition key to be set to null, but instead the queue partition key after deletion is `%s`.",
+                    messageHandle.getQueuePartitionKey()));
+        }
     }
 
     /* This method demonstrates how to delete the queue partition key from a message
      * that does not have the queue partition key set.
      */
     public void howToDeleteAbsentQueuePartitionKey(MessageHandle messageHandle) throws SolclientException {
-        assertNull(String.format("Expected to be passed a MessageHandle without a queue partition key, but "
-            + "instead, the queue partition key was set to `%s`.",
-            messageHandle.getQueuePartitionKey()),
-        messageHandle.getQueuePartitionKey());
+    	if (messageHandle.getQueuePartitionKey() != null) {
+    		throw new AssertionError(String.format("Expected to be passed a MessageHandle without a queue partition key, but "
+    	            + "instead, the queue partition key was set to `%s`.",
+    	            messageHandle.getQueuePartitionKey()));
+    	}
     }
 
 }
